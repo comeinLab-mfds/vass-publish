fc53b14be397b60b3523ad09e12215cb9d10e61f
YIUSER-PC <YIUSER-PC@yumc.net>
2021-06-17 15:21:42 +0900

    조작적정의 생성 모달팝업

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 "use strict";
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal-close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 var vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
 window.addEventListener('resize', function () {
@@ -160,3 +186,4 @@ if (rowQuery) {
     tooltip.innerHTML = "Click to copy to clipboard";
   });
 }
+
