a0520528b0254518903128e8abd46648aeda35cc
dlgywls1130 <dlgywls1130@gmail.com>
2021-06-23 14:26:33 +0900

    모달 팝업 수정중

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,41 @@
 
 
 //*modal 팝업 *//
-// Get the modal
-const modal = document.getElementById("myModal");
+var modals = document.getElementsByClassName("myModal");
+var btns = document.getElementsByClassName("btn-submit");
+var spanes = document.getElementsByClassName("modalClose");
+var funcs = [];
+ 
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("modal-close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function Modal(num) {
+  return function() {
+    btns[num].onclick =  function() {
+        modals[num].style.display = "block";
+        console.log(num);
+    };
+ 
+    spanes[num].onclick = function() {
+        modals[num].style.display = "none";
+    };
+  };
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+ 
+for(var i = 0; i < btns.length; i++) {
+  funcs[i] = Modal(i);
 }
-
-// When the user clicks anywhere outside of the modal, close it
+ 
+for(var j = 0; j < btns.length; j++) {
+  funcs[j]();
+}
+ 
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target.className == "myModal") {
+      event.target.style.display = "none";
   }
-}
+};
+
+
+
 
 
 
